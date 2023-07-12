d9b53112cfc0efd78931a339aad15e02282fc2f2
Rusbel Gaviria Devia <rgaviria@ariapsw.com>
2023-07-12 16:35:47 -0500

    Hola mundo con spring boot

---
+++ src/test/java/com/platzi/markert/PlatziMarkertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platzi.markert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatziMarkertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
