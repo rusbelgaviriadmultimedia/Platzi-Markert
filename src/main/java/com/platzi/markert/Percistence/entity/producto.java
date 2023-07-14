648eab17158f3017e282e755c34ccb15bb289c23
Rusbel Gaviria Devia <kimar041604@gmail.com>
2023-07-14 11:41:20 -0500

    Creación Producto

--- src/main/java/com/platzi/markert/Percistence/entity/producto.java
+++ src/main/java/com/platzi/markert/Percistence/entity/producto.java
@@ -1,8 +1,32 @@
 package com.platzi.markert.Percistence.entity;
 
+import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+
+import jakarta.persistence.Id;
+
+import java.io.Serializable;
+
 @Entity
 public class producto {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_producto")
     private Integer idProducto;
+
+    private String nombre;
+
+    @Column(name = "id_categoria")
+    private Integer idCategoria;
+
+    @Column(name = "codigo_barras")
+    estado codigoBarras;
+
+    @Column(name = "precio_venta")
+    private Double precioVenta;
+
+    @Column(name = "cantidad_stock")
+    private Integer cantidadStock;
+
+    private Boolean estado;
 }
