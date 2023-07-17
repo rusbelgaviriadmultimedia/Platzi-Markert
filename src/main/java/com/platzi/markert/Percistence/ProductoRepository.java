be9b25584c3700df53c5a625662f4cbb72c25acf
Rusbel Gaviria Devia <kimar041604@gmail.com>
2023-07-17 09:31:19 -0500

    Query Methods

--- src/main/java/com/platzi/markert/Percistence/ProductoRepository.java
+++ src/main/java/com/platzi/markert/Percistence/ProductoRepository.java
@@ -1,7 +1,41 @@
 package com.platzi.markert.Percistence;
 
-public class ProductoRepository {
-    private productoCrudRepository productoCrudRepository;
+import com.platzi.markert.Percistence.crud.ProductoCrudRepository;
 
-    public List<Productos>
+import java.util.List;
+import java.util.Optional;
+<<<<<<< HEAD
+
+=======
+import java.util.OptionalDouble;
+>>>>>>> 0f5f02523702369f3c177ea765096df65c513c73
+
+import com.platzi.markert.Percistence.entity.Producto;
+
+public class ProductoRepository {
+    private ProductoCrudRepository productoCrudRepository;
+
+    //metodo publico que retorna inalista de productos
+    public List<Producto> getAll() {
+        return (List<Producto>) productoCrudRepository.findAll();
+
+    }
+
+    //todos los productos se van a ordenar de manera acendente
+    public List<Producto> getByCategoria(int idCategoria) {
+        return productoCrudRepository.findByIdCatgoriaOrderByNombreAsc(idCategoria);
+    }
+
+<<<<<<< HEAD
+    //
+    public Optional<List<Producto>> getEscasos(int cantidad, boolean stado) {
+        return productoCrudRepository.findByCantidadStocklessThanAndEstado(cantidad,boolean true);
+    }
+    public Optional<Producto> getProducto(int idproducto) {
+        return productoCrudRepository.findById(idproducto);
+=======
+    public Optional<List<Producto>> getEscasos(int cantidad, boolean estado) {
+        return productoCrudRepository.findByCantidadStocklessThanAndEstado(cantidad, estado=true);
+>>>>>>> 0f5f02523702369f3c177ea765096df65c513c73
+    }
 }
--- src/main/java/com/platzi/markert/Percistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/platzi/markert/Percistence/crud/ProductoCrudRepository.java
@@ -1,9 +1,31 @@
 package com.platzi.markert.Percistence.crud;
 
+import java.util.List;
+<<<<<<< HEAD
+import java.util.Optional;
+=======
+>>>>>>> 0f5f02523702369f3c177ea765096df65c513c73
+
 import org.springframework.data.repository.CrudRepository;
+
 
 import com.platzi.markert.Percistence.entity.Producto;
 
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+<<<<<<< HEAD
+   //Manera nativa tambien se puede hacer
+    // @Query(value = "SELECT * FROM productos WHERE id _categorias = ?", nativeQuery = true )
+=======
+   //Manera nativa
+    // @Query(value = "SELECT * FROM productos WHERE id _categorias = ?", nativeQuery = true )
 
+    List<Producto> findByIdCatgoriaOrderByNombreAsc(int idCategoria);
+
+>>>>>>> 0f5f02523702369f3c177ea765096df65c513c73
+
+    List<Producto> findByIdCatgoriaOrderByNombreAsc(int idCategoria);
+
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidad_stock,boolean estado);
+
+    Optional<List<Producto>> findByCantidadStocklessThanAndEstado(int cantidad, b:true);
 }
